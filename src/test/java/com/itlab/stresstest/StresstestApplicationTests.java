9d0dc3fb3ef8ab85d3596c2e0bc266f30663a164
itlab0816 <itlab0816@gamil.com>
2021-03-19 18:49:27 +0900

    Initial commit

---
+++ src/test/java/com/itlab/stresstest/StresstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlab.stresstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StresstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
